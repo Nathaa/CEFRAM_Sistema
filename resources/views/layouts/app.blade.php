d4ab92b028cedaa5ad57b51f1537fd555299d677
Nathaa <mamita.a53@gmail.com>
2020-07-16 22:37:18 +0200

    cam

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                                                 {{ __('Logout') }}
                                             </a>
         
-                                            <form id="logout-form" action="{{ route('auth.login') }}" method="POST" style="display: none;">
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                                 @csrf
                                             </form>
                                         </div>
